04bd4fa143cc6a73bb02c59ad3915e177bc7b16d
viraj0908 <chintaviraj09@gmail.com>
2026-06-09 23:56:03 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int f(int n){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            n=n/10;
+            sum+=(d*d);
+        }
+        return sum;
+    }
+    int helper(int n){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum+=d;
+            n=n/10;
+        }
+        return sum;
+    }
+public:
+    bool isHappy(int n) {
+        int x=n;
+        map<int,int> hash;
+        while(1){
+            if(hash.count(x)) break;
+            hash[x]=1;
+            x=f(x);
+            cout<<x<<endl;
+            if(x==1) return true;           
+        }
+        return false;
+        
+    }
+};
